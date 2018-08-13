f74cc93aae021e780be9f60fac6007d71392fe59
Kavin <kavin851018@gmail.com>
2018-08-13 19:26:46 +0200

    more layout

--- app/src/main/java/com/example/mis108/careapplication/checkInfoActivity.java
+++ app/src/main/java/com/example/mis108/careapplication/checkInfoActivity.java
@@ -8,6 +8,6 @@ public class checkInfoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_check_info);
+        setContentView(R.layout.activity_homepage);
     }
 }
